b4ebd412bd91cd384998f0326aeef45dff2c9e1d
taesubyun <taesubyun@icloud.com>
2022-10-14 14:49:39 +0900

    Create componets

---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+function Todo(props) {
+  return (
+    <div className='card'>
+      <h2>타이틀</h2>
+      <div className='actions'>
+        <button className='btn'>삭제하기</button>
+      </div>
+    </div>
+  );
+}
+export default Todo;
